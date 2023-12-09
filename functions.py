507d08a44e9ca012692660ab3b7ace40cb2e8105
Chakraless <zachiinferno1@gmail.com>
2023-12-09 15:21:09 -0500

    Removed functions.py

--- functions.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-characters = string.ascii_letters + string.digits + string.punctuation
-password_length = 12 # Define the length of the password
-password = "" # Initialize an empty string to store the password
-for index in range(password_length):
-    password += random.choice(characters)
-print(f"Password generated: {password}")
